fe1284fe92964ca9521e455baa11a09bdc1009ca
sojeongHwang <wjdth01@gmail.com>
2024-08-28 12:55:56 +0900

    [코드 추가] 구글 search console 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,10 @@ import { Analytics } from "@vercel/analytics/react";
 export const metadata: Metadata = {
   title: "비디오 자막 추출",
   description: "비디오의 자막 추출 사이트 입니다.",
+  verification: {
+    google:
+      "google-site-verification=n0RL4qu61jFMLgt73pEtMbAja0xezkHWx0vU9gkyy3o",
+  },
 };
 
 export default function RootLayout({
